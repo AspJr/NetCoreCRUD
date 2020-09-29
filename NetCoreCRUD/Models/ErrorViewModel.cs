2099eb4ff5d45ed91c63e71268efda63af07bc58
AspJr <asepmaulana7779@gmail.com>
2020-09-28 20:00:10 -0700

    FirstCommit

---
+++ NetCoreCRUD/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NetCoreCRUD.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
